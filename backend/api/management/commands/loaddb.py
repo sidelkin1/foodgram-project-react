a4ba57d7434269d3876a03e44a73ab11a12c5538
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2022-11-18 18:07:53 +0400

    Added data and command to load into db

---
+++ backend/api/management/commands/loaddb.py
@@ -0,0 +1,88 @@
+import datetime
+import shutil
+from pathlib import Path
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+class Command(BaseCommand):
+    help = 'Load Foodgram database'
+
+    DB_URI = (
+        'postgresql://'
+        f'{settings.DATABASES["default"]["USER"]}'
+        f':{settings.DATABASES["default"]["PASSWORD"]}'
+        f'@{settings.DATABASES["default"]["HOST"]}'
+        f':{settings.DATABASES["default"]["PORT"]}'
+        f'/{settings.DATABASES["default"]["NAME"]}'
+    )
+
+    BASE_DIR = Path(settings.BASE_DIR)
+
+    DB_TABLES = {
+        'auth_user.csv': 'auth_user',
+        'recipes_ingredient.csv': 'recipes_ingredient',
+        'recipes_tag.csv': 'recipes_tag',
+        'recipes_recipe.csv': 'recipes_recipe',
+        'recipes_recipe_tags.csv': 'recipes_recipe_tags',
+        'recipes_recipeingredient.csv': 'recipes_recipeingredient',
+        'recipes_favorite.csv': 'recipes_favorite',
+        'recipes_purchase.csv': 'recipes_purchase',
+    }
+
+    def handle(self, *args, **options):
+        try:
+            engine = create_engine(self.DB_URI)
+
+            with engine.connect().execution_options(autocommit=True) as conn:
+                self.stdout.write(
+                    self.style.SUCCESS('Успешное подключение к БД')
+                )
+
+                for file_name, table_name in self.DB_TABLES.items():
+                    self.stdout.write(
+                        self.style.NOTICE(
+                            f'Обработка таблицы {table_name}... '
+                        ),
+                        ending='',
+                    )
+
+                    path = self.BASE_DIR.joinpath('data', file_name)
+                    df = pd.read_csv(path)
+
+                    if table_name == 'auth_user':
+                        df = df.assign(
+                            password='12345',
+                            last_login=pd.NaT,
+                            is_superuser=False,
+                            is_staff=False,
+                            is_active=True,
+                            date_joined=datetime.datetime.now(),
+                        )
+                    elif table_name == 'recipes_recipe':
+                        path = self.BASE_DIR.joinpath('data')
+                        media = self.BASE_DIR.joinpath('media', 'recipes')
+                        media.mkdir(parents=True, exist_ok=True)
+                        for image in path.glob('*.jpg'):
+                            shutil.copy(image, media)
+
+                    df.to_sql(
+                        name=table_name,
+                        con=conn,
+                        if_exists='append',
+                        index=False,
+                        index_label='id'
+                    )
+
+                    self.stdout.write(
+                        self.style.SUCCESS('OK')
+                    )
+
+        except Exception as error:
+            raise CommandError(error) from error
+        finally:
+            engine.dispose()
--- infra/migrate.sh
+++ infra/migrate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker-compose exec backend python manage.py migrate
-# docker-compose exec backend python manage.py loaddb
-# docker-compose exec backend python manage.py createsuperuser2 --noinput
+docker-compose exec backend python manage.py loaddb
+docker-compose exec backend python manage.py createsuperuser --no-input
 docker-compose exec backend python manage.py collectstatic --no-input
